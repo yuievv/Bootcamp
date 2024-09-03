7c4fd9b2578fef19b6e6e2c04c6f4c603d26f460
yuievv <99083027@mydavinci.nl>
2024-09-03 12:08:23 +0200

    Gemiddelde

---
+++ gemiddelde.py
@@ -0,0 +1,8 @@
+var1 = float(input("Voer het eerste getal in: "))
+var2 = float(input("Voer het tweede getal in: "))
+var3 = float(input("Voer het derde getal in: "))
+
+
+gemiddelde = (var1 + var2 + var3) / 3
+
+print("Het gemiddelde is:", gemiddelde)
