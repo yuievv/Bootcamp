ab0cb56a7665da6ef1d2a5e7b66606aa198264f4
yuievv <99083027@mydavinci.nl>
2024-09-03 12:11:28 +0200

    Aantal tekens

--- bc_l3_doemee.py
+++ bc_l3_doemee.py
@@ -1,16 +1,10 @@
-# Wat gaat er gebeuren?
 getal1 = 25
 getal2 = 10
 print(getal1/getal2)
 
-
-# we gaan hier een variable inbouwen
 naam = input("Voer je naam in: ")
 print('Hallo ' + naam + ', ik leer nu programmeren.')
 
-
-# we schrijven code dat de gebruiker vraagt om een woord in te voeren
-# en vervolgens de volgende zin print:
 woord = input("Vul een woord in: ")
 aantal_tekens = len(woord)
 print(f"Het woord is: {woord} en bestaat uit {aantal_tekens} tekens.")
