912ae922b2bb5ded96947a1799cc2b8af9ab0c44
yuievv <99083027@mydavinci.nl>
2024-09-05 00:14:18 +0200

    verhaal

--- les 3/verhaal.py
+++ les 3/verhaal.py
@@ -1,18 +1,17 @@
-mooie = input("Wat voor soort dag is het? ")  
-dordrecht = input("In welke stad gebeurd het verhaal? ")  
-Julia = input("Wie is het hoofdpersoon? ") 
-supermarkt = input("Waar gaan hoofdpersoon naar toe? ")  
-oude = input("Hoe zou je de vrouw beschrijven die de Julia ziet? ") 
-kaarsen = input("Wat verkoopt de vrouw? ") 
-plein = input("Wat voor plek komt de hoofdpersoon tegen? ")  
-dansen = input("Wat doet de groep mensen? ")  
+mooie = input("Wat voor soort dag is het?: ")
+dordrecht = input("In welke stad speelt het verhaal zich af?: ")
+julia = input("Wie is de hoofdpersoon?: ")
+supermarkt = input("Waar gaat de hoofdpersoon naartoe?: ")
+oude = input("Hoe zou je de vrouw beschrijven die de hoofdpersoon ziet?: ")
+kaarsen = input("Wat verkoopt de vrouw?: ")
+park = input("Wat voor plek komt de hoofdpersoon tegen?: ")
+dansen = input("Wat doet de groep mensen?: ")
 
+verhaal = (
+    f"Het was een {mooie} dag in {dordrecht}. {julia} liep door de straten, op zoek naar een {supermarkt}. "
+    f"Plotseling zag {julia} een {oude} vrouw die {kaarsen} verkocht. {julia} besloot om een paar te kopen en liep verder. "
+    f"Toen {julia} bij een {park} kwam, zag zij een groep mensen die {dansen}. "
+    f"{julia} besloot om mee te doen en had de tijd van haar leven."
+)
 
-verhaal = 
-    f"Het was een {mooie} dag in {dordrecht}. {Julia} liep door de straten, op zoek naar een {supermarkt}. "
-    f"Plotseling zag {Julia} een {oude} vrouw die {kaarsen} verkocht. {Julia} besloot om paar van te kopen en liep verder. "
-    f"Toen {Julia} bij een {plein} kwam, zag zij een groep mensen die {dansen}. "
-    f"{Julia} besloot om mee te doen en had de tijd van haar leven.
-
-
-print(verhaal)
+print("\n" + verhaal)
