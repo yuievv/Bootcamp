39659fd74159863e801d7f45cbd92c8518c41f85
yuievv <99083027@mydavinci.nl>
2024-09-05 13:40:04 +0200

    opdrachten

---
+++ les 6/bc_l6_doemee.py
@@ -0,0 +1,16 @@
+# we gaan aan de lijst met getallen 
+# het getal 6 tevoegen en dan alles optellen
+getallen = [1, 2, 3, 4, 5]
+
+# we gaan checken of in deze lijst met sporten
+# een bepaalde sport zit en welke indec die heeft
+sporten = ["voetbal", "basketbal", "tennis", "zwemmen", "honkbal", "volleybal", "atletiek"]
+print(sporten)
+if "zwemmen" in sporten:
+    print("Ok")
+else:
+    print("NOTOK")
+
+# we gaan uit de volgende lijst met autos 1 halen en tonen
+# en dan de rest sorteren
+autos = ['BMW', 'Audi', 'Ford', 'Volvo']
---
+++ les 7/bc_l7_doemee.py
@@ -0,0 +1,27 @@
+# hiermee gaan we een if schrijven die als de leeftijd groter of
+# gelijk is aan 18 en iemand een rijbewijs heeft print "Je mag autorijden."
+# anders print de code "Je mag niet autorijden."
+age = 25
+has_license = True
+
+
+# hiermee gaan we een if schrijven die als het weekend of
+# vakantie is print "Je hoeft vandaag niet te werken."
+# anders print de code "Het is een gewone werkdag."
+is_weekend = True
+is_holiday = False
+
+
+# hiermee gaan we een if schrijven die als het niet regent
+# print "Je kunt buiten gaan wandelen."
+# anders print de code "Je moet binnen blijven."
+is_raining = False
+
+
+# hiermee gaan we een if schrijven die als
+# de tempratuur boven de 20 is of het is zonnig en er is geen jas
+# print "Het is een mooie dag om zonder jas naar buiten te gaan."
+# anders print de code "Je hebt misschien een jas nodig."
+temperature = 22
+is_sunny = True
+has_jacket = False
