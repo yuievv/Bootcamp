66d23dc1904e5273a4f7fa3a707db87b58f90f70
yuievv <99083027@mydavinci.nl>
2024-09-05 10:56:50 +0200

    les 1

---
+++ les 1/test.py
@@ -0,0 +1,7 @@
+name = input("Wat is je naam? ")
+age = int(input("Hoe oud ben je? "))
+
+current_year = 2024  
+year_when_100 = current_year + (100 - age)
+
+print(f"Hallo {name}, je wordt 100 jaar oud in het jaar {year_when_100}.")
